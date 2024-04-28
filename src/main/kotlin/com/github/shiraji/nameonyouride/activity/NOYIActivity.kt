0a4aecebc41ec62e2dcc4fc5291e88804cc431f9
shiraji <isogai.shiraji@gmail.com>
2024-04-28 11:58:46 +0900

    update

--- src/main/kotlin/com/github/shiraji/nameonyouride/activity/NOYIActivity.kt
+++ src/main/kotlin/com/github/shiraji/nameonyouride/activity/NOYIActivity.kt
@@ -11,40 +11,71 @@ import java.awt.BorderLayout
 import java.awt.Font
 import java.awt.event.WindowEvent
 import java.awt.event.WindowFocusListener
-import javax.swing.JLabel
-import javax.swing.JRootPane
-import javax.swing.SwingUtilities
+import javax.swing.*
 
 class NOYIActivity : StartupActivity {
     override fun runActivity(project: Project) {
         val frame = WindowManager.getInstance().getFrame(project)
-        frame?.addWindowFocusListener(NOYIWindowFocusListener(project, frame))
-
-
         val parent = frame?.rootPane
         val rootPane: JRootPane = UIUtil.findComponentOfType(parent, JRootPane::class.java) ?: return
 
+        // add background color
+//        val label = object : JLabel(project.name) {
+//            override fun paintComponent(g: Graphics) {
+//                g.color = background
+//                g.fillRect(0, 0, width, height)
+//                super.paintComponent(g)
+//            }
+//
+//            override fun isOpaque(): Boolean {
+//                return false
+//            }
+//        }
         val label = JLabel(project.name)
-        rootPane.add(label, BorderLayout.CENTER, 1)
+        rootPane.add(label, BorderLayout.CENTER, 0)
         label.isVisible = false
         label.setSize(rootPane.size.width, rootPane.size.height)
         label.foreground = JBColor.RED
         label.font = Font("Arial", Font.PLAIN, 100)
-        label.horizontalAlignment = JLabel.CENTER
+        label.horizontalAlignment = SwingConstants.RIGHT
+        label.verticalAlignment = SwingConstants.TOP
 
-        // in case the frame is never focused
-        label.isVisible = frame?.isFocused != true
+        if (frame?.isFocused == true) {
+            label.isVisible = false
+        } else {
+            label.isVisible = true
+        }
 
         frame?.addWindowFocusListener(object : WindowFocusListener {
-            override fun windowGainedFocus(p0: WindowEvent?) {
+            val visibleTimer = Timer(300) {
+                println("visible timer")
+                label.isVisible = true
+                label.repaint()
+                (it.source as? Timer)?.stop()
+            }
+            val nonVisibleTimer = Timer(300) {
+                println("non visible timer")
                 label.isVisible = false
+                label.repaint()
+                (it.source as? Timer)?.stop()
+            }
+
+            override fun windowGainedFocus(p0: WindowEvent?) {
+                println("windowGainedFocus ${p0?.paramString()}")
+                label.isVisible = false
+                visibleTimer.stop()
+                nonVisibleTimer.start()
             }
 
             override fun windowLostFocus(p0: WindowEvent?) {
+                println("windowLostFocus ${p0?.paramString()}")
+
                 val opposite = p0?.oppositeWindow
                 if (opposite == null || SwingUtilities.getAncestorOfClass(IdeFrame::class.java, opposite) != frame) {
-                    label.isVisible = true
                     label.setSize(rootPane.size.width, rootPane.size.height)
+                    label.isVisible = true
+                    visibleTimer.start()
+                    nonVisibleTimer.stop()
                 }
             }
         })
